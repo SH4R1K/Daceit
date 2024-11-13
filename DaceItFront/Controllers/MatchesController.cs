1c8437c3f4c4058135c91d2ff6c07dfa9b4658a4
0109-05 <0109-05@softsol.local>
2024-11-13 14:12:31 +0300

    add registration, changeBackgroundColor, adn some pages

--- DaceItFront/Controllers/MatchesController.cs
+++ DaceItFront/Controllers/MatchesController.cs
@@ -49,6 +49,10 @@ namespace DaceItFront.Controllers
             return View();
         }
 
+        public IActionResult MatchView()
+        {
+            return View();
+        }
         // POST: Matches/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
